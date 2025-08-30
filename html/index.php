5bac7b2a0536da00aeee864a1a2bd68e9604c02e
jonibeckch <159580839+jonibeckch@users.noreply.github.com>
2025-08-30 16:00:59 +0200

    Create index.php

---
+++ html/index.php
@@ -0,0 +1,22 @@
+<p style="font-family: Arial, Verdana, Helvetica;">
+Temperatur - relative und absolute Luftfeuchtigkeit<br>
+Sollwert absolute Feuchte: 7.3 - 11 g / m<sup>3</sup> (40 - 60 % relative Feuchte bei 21&deg;C)
+<br><br><br>
+How humid is it today?
+<br>
+<img src="af2.png">
+<br>
+Longterm:
+<br>
+<img src="af.png">
+<br>
+<img src="temphum.png">
+<br>
+<br>
+<img src="temp.png">
+<br>
+
+<br>
+<br>
+</p>
+
