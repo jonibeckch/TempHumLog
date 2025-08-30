2dbd91f6ef3893ea94c5a2de44489426a3fbcf1c
jonibeckch <159580839+jonibeckch@users.noreply.github.com>
2025-08-30 15:59:00 +0200

    Rename temphum_renew.sh to temphum/temphum_renew.sh

---
+++ temphum/temphum_renew.sh
@@ -0,0 +1,2 @@
+sudo rm /home/pi/temphum/plot-temphum2.dat
+sudo touch /home/pi/temphum/plot-temphum2.dat
--- temphum_renew.sh
+++
@@ -1,2 +0,0 @@
-sudo rm /home/pi/temphum/plot-temphum2.dat
-sudo touch /home/pi/temphum/plot-temphum2.dat
